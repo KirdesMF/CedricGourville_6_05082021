992e918c4c466b46852103d6d211b4a6db9bba60
Cedric Gourville <cedgourville@gmail.com>
2021-08-05 23:22:44 +0200

    ⚗️ try backend packages

--- backend/app.ts
+++ backend/app.ts
@@ -1,26 +1,44 @@
-import { graphqlHTTP } from 'express-graphql';
-import { buildSchema } from 'graphql';
 import express from 'express';
+import dotenv from 'dotenv';
 
-const schema = buildSchema(`
-   type Query {
-      hello: String
-   }
-`);
+dotenv.config();
 
-const root = {
-   hello: () => `Hello World`,
-};
-
+const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(
-   '/',
-   graphqlHTTP({
-      schema,
-      rootValue: root,
-      graphiql: true,
-   })
-);
+app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+   );
+   res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+   );
+   next();
+});
 
-app.listen(4000);
+app.get('/', (req, res) => res.send('Hello'));
+app.post('/api/auth/login', (req, res) => {
+   console.log(req.params);
+   return res.send({ userId: '', token: '' });
+});
+
+const fakeSauce = {
+   userId: '123',
+   name: 'fake',
+   manufacturer: 'fakeManu',
+   description: 'lorem',
+   mainPepper: '',
+   imageUrl: '',
+   heat: 2,
+   likes: 12,
+   dislikes: 20,
+   usersLiked: ['123', '245'],
+   usersDislikes: ['589', '789'],
+};
+app.get('/api/sauces', (req, res) => {
+   res.send([fakeSauce]);
+});
+app.listen(port, () => console.log(`API hosted: http://localhost:${port}`));
